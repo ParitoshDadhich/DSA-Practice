b7cfb4651576ac811536b1906bc52db8aa9c2da0
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-05-21 17:18:22 +0530

    Time: O(number of bits in an integer)

--- BIT MANIPULATION/reverse_all_bits.java
+++ BIT MANIPULATION/reverse_all_bits.java
@@ -17,3 +17,20 @@ public class Solution {
         return res;
     }
 }
+
+// ANOTHER WAY OF WRITING ABOVE CODE
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0, lsb=0;
+        for(int i=31; i>=0; i--){
+            lsb = n & 1;
+            lsb <<= i;
+            res |= lsb;
+            n >>= 1;
+        }
+
+        return res;
+    }
+}
