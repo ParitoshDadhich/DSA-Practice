60c053f60fa0feaf7372cfb946456fc57e5afdda
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-08-11 04:30:42 +0530

    Update longest-sub-array-with-sum-k.java

--- Sliding Window/Variable window size/longest-sub-array-with-sum-k.java
+++ Sliding Window/Variable window size/longest-sub-array-with-sum-k.java
@@ -25,6 +25,45 @@ class Solution{
 // space: O(1)
 //   THIS APPROACH WILL ONLY VALID FOR +VE NUMBERS
 
+
+class Solution{
+    public static int lenOfLongSubarr (int arr[], int n, int k) {
+        int max = 0;
+        int i=0, j=0, tempSum=0;
+        while(j < n){
+            tempSum += arr[j];
+            if(tempSum < k){
+                // j++
+            }
+            else if(tempSum == k){
+                max = Math.max(max, j-i+1);
+                    // j++;
+            }                
+            // we will be keep on subtracting until tempSum become less than or equal to k
+            else if(tempSum > k){
+                while(tempSum > k){
+                    tempSum = tempSum - arr[i];
+                    i++;
+                }
+                
+                // while subtracting tempSum by arr[i], one codition will come when TempSum == k, consider eg: [1 4 3 3 3 7], k = 16
+                if(tempSum == k){
+                    max = Math.max(max, j-i+1);
+                }
+                
+                // j++;
+            }
+
+            // Since in every condition I am incrementing j by 1 so commenting there and adding here
+            j++;
+        }
+        return max;
+    }
+}
+
+
+
+// another way of writing the above approach
 class Solution{
     public static int lenOfLongSubarr (int arr[], int n, int k) {
         int max = 0;
