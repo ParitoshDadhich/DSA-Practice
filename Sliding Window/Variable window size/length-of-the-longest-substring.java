aba580322b4093e3d57afcc170b86563f136affb
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-08-12 19:18:25 +0530

    Update length-of-the-longest-substring.java
    solved using 4 methods

--- Sliding Window/Variable window size/length-of-the-longest-substring.java
+++ Sliding Window/Variable window size/length-of-the-longest-substring.java
@@ -88,3 +88,48 @@ class Solution {
         return max;
     }
 }
+
+
+// method 4 -> different way to use above approach -> AADITY VERMA APPROACH
+// time: o(n)
+// space: o(n)
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int size = s.length();
+        int i=0;
+        int j=0;
+        int max = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        while(j<size){
+            // calculations
+            char ch = s.charAt(j);
+            if(map.containsKey(ch))
+                map.put(ch, map.get(ch)+1);
+            else
+                map.put(ch, 1);
+            
+            // hitting the condition
+            if(map.size() == j-i+1){
+                max = Math.max(max, j-i+1);
+                j++;
+            }
+            
+            // when window contains duplicate elements
+            else if(map.size() < j-i+1){
+                while(map.size() < j-i+1){
+                    char c = s.charAt(i);
+                    
+                    if(map.containsKey(c))
+                        map.put(c, map.get(c)-1);
+                    if(map.get(c) == 0)
+                        map.remove(c);
+                    i++;
+                }
+                j++;
+            }
+        }
+        return max;
+    }
+}
