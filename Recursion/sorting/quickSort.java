0515a9104c780ac5236fa09850041fa349933384
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-08-13 02:55:32 +0530

    Create quickSort.java

---
+++ Recursion/sorting/quickSort.java
@@ -0,0 +1,35 @@
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low >= high)
+            return;
+        
+        int s = low;
+        int e = high;
+        int mid = low + (high-low)/2;
+        int pivot = arr[mid];
+        
+        while(s <= e){
+            while(arr[s] < pivot)
+                s++;
+            
+            while(arr[e] > pivot)
+                e--;
+            
+            if(s <= e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                
+                s++;
+                e--;
+            }
+        }
+        
+        quickSort(arr, low, e);
+        quickSort(arr, s, high);
+    }
+}
