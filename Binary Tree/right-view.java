c4a74d5362059c473cd7f6c7502deffd575584ad
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-09-01 01:35:19 +0530

    solved using 2 approaches

--- Binary Tree/right-view.java
+++ Binary Tree/right-view.java
@@ -1,8 +1,9 @@
-// time o(n)
-// space o(n)
+// method 1
+// using level order traversal
+// Time o(n) and space o(n)
 
-
-class Solution {
+	
+    class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
@@ -27,3 +28,49 @@ class Solution {
         return ans;
     }
 }
+
+// method 2
+// using preorder traversal
+// time o(n) and space o(h), h is the height of the binary tree
+
+
+	class Solution {
+        
+    boolean check[];
+    List<Integer> rightSideView(TreeNode root)
+    { 
+        if(root == null)
+            return new ArrayList<>();
+        
+     
+      int height = depth(root);
+      check = new boolean[height];
+      
+      ArrayList<Integer> list = new ArrayList<>();
+      leftView(root, list, 0);
+      
+      return list;
+    }
+    
+    private void leftView(TreeNode root, ArrayList<Integer> list, int level){
+        if(root == null) return;
+        
+        if(check[level] == false){
+            list.add(root.val);
+            check[level] = true;
+        }
+        
+        // first traverse right and then left
+        leftView(root.right, list, level+1);
+        leftView(root.left, list, level+1);
+        
+        return;
+    }
+    
+    private int depth(TreeNode root){
+        if(root == null)
+            return 0;
+        
+        return 1 + Math.max(depth(root.left), depth(root.right));
+    }
+	}
